f18989aa09d78ef0b76e0fb59fe81b54030712b5
z7pz <sdfygamer@gmail.com>
2024-08-19 02:42:50 +0300

    fist

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,10 +3,10 @@ import "./globals.css"
 export default function Loading() {
 	return (
 		<div className="flex justify-center items-center h-screen w-screen">
-			<div className="select-none image-container relative overflow-hidden">
+			<div className="image-container relative overflow-hidden">
 			<img
 				width={500}
-				className="object-cover select-none pointer-events-none"
+				className="object-cover pointer-events-none"
 				src="/loading.gif"
 			/>
 			</div>
